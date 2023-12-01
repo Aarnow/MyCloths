84a32d4e31ddfae582a6f12c49d88185232eebc4
Aarnow <arnaudfgr.dev@gmail.com>
2023-12-02 00:54:02 +0100

    Initial Project Files

---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿using Life;
+using System;
+
+namespace MyCloths
+{
+    public class Main : Plugin
+    {
+        public Main(IGameAPI api) :base(api)
+        {
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            Console.WriteLine($"Plugin \"MyCloths\" initialisé avec succès.");
+        }
+    }
+}
